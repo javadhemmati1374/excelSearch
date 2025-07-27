1641de4f4f0c3bfa21b939eab604c8e14fa61987
javadhemmati1374 <javadhemati1374@gmail.com>
2025-07-28 00:33:53 +0330

    Update route.ts

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,7 +1,7 @@
 // src/app/api/upload/route.ts
 
 import { NextResponse } from "next/server";
-import { PrismaClient } from "../../../../src/generated/prisma";
+import { PrismaClient } from "@/generated/prisma";
 import ExcelJS from "exceljs";
 import { Readable } from "stream";
 
